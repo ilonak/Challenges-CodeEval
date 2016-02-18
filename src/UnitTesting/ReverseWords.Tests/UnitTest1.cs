69f64b85e3556c7f2507a49b92e4b52cf46e669e
Ilona Kravets <ilona.kravets@hotmail.com>
2016-02-17 23:19:28 -0500

    Added ReverseWords.Tests project.

---
+++ src/UnitTesting/ReverseWords.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseWords.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
