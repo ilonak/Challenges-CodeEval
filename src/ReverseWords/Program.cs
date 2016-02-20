d833ef45a55e0da72c466d9ddb43c2132751ff40
Ilona Kravets <ilona.kravets@hotmail.com>
2016-02-20 08:23:03 -0500

    Added initial reversed print functionality.

--- src/ReverseWords/Program.cs
+++ src/ReverseWords/Program.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace ReverseWords
+
+namespace Words_in_file_reversed
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			string path = "D:/Training/sum-of-primes/test-input.txt";
+			string line;
+
+			StreamReader reader = new StreamReader(path);
+			string[] separator = { " ", ",", ".", "!", "?", ";", ":" };
+
+			while ((line = reader.ReadLine()) != null)
+			{
+				if (line != "")
+				{
+					string[] words = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+					int i;
+					i = words.Length - 1;
+					while (i >= 0)
+					{
+						Console.Write("{0}", words[i]);
+						if (i != 0)
+						{
+							Console.Write(" ");
+						}
+						i--;
+					}
+					Console.Write("\n\r");
+				}
+
+
+				//for (int i = words.Length - 1; i >= 0; i--)
+				//{
+				//	Console.WriteLine("{0}", );
+				//}
+			}
+			sr.Close();
+			Console.ReadLine();
+
 		}
 	}
 }
