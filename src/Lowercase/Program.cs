d4c0ad5b4e0212c2eacca16e8b3eb9a552a9f465
Ilona Kravets <ilona.kravets@hotmail.com>
2016-02-21 15:15:19 -0500

    Added initial project for Lowercase challenge.

---
+++ src/Lowercase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lowercase
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		static void ToLowercase(string path)
+		{
+			try
+			{
+				StreamReader sr = new StreamReader();
+				string line;
+				while ((line = sr.ReadLine()) != null)
+				{
+					Console.WriteLine(line.ToLower());
+				}
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+
+		}
+
+	}
+}
