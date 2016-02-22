e11b52301195a4d56bc2c554d6b7d8ef912153d7
Ilona Kravets <ilona.kravets@hotmail.com>
2016-02-21 22:33:57 -0500

    Added first test for not existing file.

---
+++ src/UnitTesting/ReverseWords.Tests/PrintReversedWordsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseWords.Tests
+{
+	[TestClass]
+	public class PrintReversedWordsTest
+	{
+
+		[TestMethod]
+		public void PrintReversedWords_NotExistingFile_ExpectedNothing()
+		{
+			Program.PrintReversedWords("not exist");
+		//	Assert.AreEqual(expected, actual, "Wrong sum returned");
+		}
+
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
