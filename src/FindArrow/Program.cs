192589cb2b5306c410221971e4bbb872a3660281
Ilona Kravets <ilona.kravets@hotmail.com>
2016-02-27 23:14:33 -0500

    Implemented AnalyzeOneLine. Tests one and zero pass.

--- src/FindArrow/Program.cs
+++ src/FindArrow/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FindArrow
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length > 0 && File.Exists(args[0]))
+				AnalyzeFileForArrows(args[0], Console.Write);
+		}
+
+		public static void AnalyzeFileForArrows(string path, Action<string> Write)
+		{
+			try
+			{
+				string line;
+				StreamReader reader = new StreamReader(path);
+				//				string[] separator = { " ", ",", ".", "!", "?", ";", ":" };
+
+				while ((line = reader.ReadLine()) != null)
+				{
+					int count = AnalyzeOneLine(line);
+					Write(count.ToString());
+					Write(Environment.NewLine);
+					Console.ReadLine();
+				}
+				reader.Close();
+			}
+			catch (Exception e)
+			{
+				Write("The file could not be read." + Environment.NewLine);
+			}
+		}
+
+		public static int AnalyzeOneLine(string line)
+		{
+			int count = 0;
+			if (!String.IsNullOrWhiteSpace(line) && line.Length > 4)
+			{
+				int i = 0;
+				//bool patternStarted = false;
+				while (i < line.Length - 4)
+				{
+					//string ch = line[i].ToString();
+					if (line[i].ToString() == ">")
+					{
+						if (line[i + 1].ToString() == ">" && line[i + 2].ToString() == "-" && line[i + 3].ToString() == "-" && line[i + 4].ToString() == ">")
+						{
+							count++;
+							i++;
+						}
+					}
+					if (line[i].ToString() == "<")
+					{
+						if (line[i + 1].ToString() == "-" && line[i + 2].ToString() == "-" && line[i + 3].ToString() == "<" && line[i + 4].ToString() == "<")
+						{
+							count++;
+							i++;
+						}
+					}
+					i++;
+				}
+			}
+			return count;
 		}
 	}
 }
