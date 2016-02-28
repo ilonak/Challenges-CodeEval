a0cf6e1e45890549f4df5d03b3d4c87e50a80363
Ilona Kravets <ilona.kravets@hotmail.com>
2016-02-27 21:08:04 -0500

    Created boiler plate code for unit testing.

---
+++ src/UnitTesting/FindArrow.Tests/FindArrowTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindArrow.Tests
+{
+	[TestClass]
+	public class FindArrowTest
+	{
+		private TestContext testContextInstance;
+
+		public TestContext TestContext
+		{
+			get { return testContextInstance; }
+			set { testContextInstance = value; }
+		}
+
+	//	[TestMethod]
+	//	public void PrintReversedWords_NotExistingFile_ExpectedErrorMessage()
+	//	{
+	//		string actual = string.Empty;
+	//		string expected = "The file could not be read." + Environment.NewLine;
+	//		//Program.PrintReversedWords("not exist", (s) => { actual += s; });
+	//		Assert.AreEqual(expected, actual);
+	//	}
+	//
+	//	[TestMethod]
+	//	[DeploymentItem("input.txt")]
+	//	public void PrintReversedWords_SampleFile_ExpectedNoErrors()
+	//	{
+	//		string actual = string.Empty;
+	//		string expected = "World Hello"+ Environment.NewLine + "CodeEval Hello" + Environment.NewLine;
+	//		//Program.PrintReversedWords("input.txt", (s) => { actual += s; });
+	//		Assert.AreEqual(expected, actual);
+	//	}
+	}
+}
