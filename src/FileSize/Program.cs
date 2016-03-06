a87cfbd9aec63f41d552061c8d35c8c56c820c90
Ilona Kravets <ilona.kravets@hotmail.com>
2016-03-05 19:08:01 -0500

    FileSize.sln created, implemented, submitted.

---
+++ src/FileSize/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace StringToLower
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length > 0 && File.Exists(args[0]))
+			{
+				FileInfo file = new FileInfo(args[0]);
+				long fileSize = file.Length;
+				Console.WriteLine(fileSize.ToString());
+				//Console.ReadLine();
+			}
+		}
+	}
+}
