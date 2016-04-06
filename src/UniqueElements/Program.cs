d739415ab584a206efd78dc67b8c6df7b1bf106e
Ilona Kravets <ilona.kravets@hotmail.com>
2016-04-05 20:05:06 -0400

    Implemented print of unique elements in sorted array.

---
+++ src/UniqueElements/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UniqueElements
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length > 0 && File.Exists(args[0]))
+			{
+				ProcessFile(args[0], Console.Write);
+			}
+			Console.ReadLine();
+		}
+
+		private static void ProcessFile(string path, Action<string> Write)
+		{
+			try
+			{
+				StreamReader sr = new StreamReader(path);
+				string line;
+				while ((line = sr.ReadLine()) != null && !String.IsNullOrWhiteSpace(line))
+				{ 
+					char delimiter = ',';
+					string[] numbers = line.Split(delimiter);
+					Write(numbers[0]);
+
+					for (int i = 1; i < numbers.Length; i++)
+					{
+						if (numbers[i] != numbers[i - 1])
+						{
+							Write("," + numbers[i]);
+						}
+					}
+					Write(Environment.NewLine);
+				}
+			}
+			catch (Exception e)
+			{
+				Write("File cannot be processed" + e);
+			}
+						
+		}
+	}
+}
