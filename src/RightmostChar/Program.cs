7b97e481c0f2985d674fb2209a4de3d53322a832
Ilona Kravets <ilona.kravets@hotmail.com>
2016-04-06 22:28:45 -0400

    submitted

---
+++ src/RightmostChar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace RightmostChar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length > 0 && File.Exists(args[0]))
+			{
+				ProcessFile(args[0], Console.Write);
+				Console.ReadLine();
+			}
+		}
+
+		private static void ProcessFile(string path, Action<string> Write)
+		{
+			try
+			{
+				StreamReader sr = new StreamReader(path);
+				string line = "";
+				while ((line = sr.ReadLine()) != null)
+				{
+					if (!String.IsNullOrWhiteSpace(line))
+					{
+						char[] delimiter = {','};
+						string[] separatedLine = line.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+						string words = separatedLine[0];
+						string character = separatedLine[1];
+						int position = words.LastIndexOf(character);
+
+						Write(position.ToString());
+						Write(Environment.NewLine);
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				Write(e.ToString());
+			}
+		}
+	}
+}
